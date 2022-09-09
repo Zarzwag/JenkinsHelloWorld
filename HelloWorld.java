0f626d9167543474b00474f4b0ab6d9b17371daf
Luis Fernando Zarza <luis.zarza.progra@gmail.com>
2022-09-09 09:29:19 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-	System.out.println(".....*****AHORA SI CAMBIE EL MENSAJE LETS GO MY NIBBA!!*****.....");
+	System.out.println(".....*****TRYING WEBHOOKS :D!!*****.....");
 	}
 }
